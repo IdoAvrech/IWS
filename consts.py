53465e526058f13813eaf883b470a24a1417b7ef
Ido Avrech <ido05469@gmail.com>
2021-09-16 02:16:08 +0300

    Add seperator and http consts

---
+++ consts.py
@@ -0,0 +1,13 @@
+"""
+ Author: Ido
+ Date: 27/08/21
+ Purpose: Be the main cmd class to use in the project, disable bad cmd2 features
+"""
+
+SEPARATOR = "-" * 50
+PHP_SEPARATOR = f"echo(\"{SEPARATOR}\");\n"
+
+HTTP_METHOD = "post"
+HTTP_VARIABLE = "test"
+
+url = "http://localhost/index.php"
