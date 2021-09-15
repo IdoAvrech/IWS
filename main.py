4c584c2f6e52fb90ef88a6292c9d333d5f24ea5e
Ido Avrech <ido05469@gmail.com>
2021-09-15 20:27:40 +0300

    init main, execute webshell

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Start the webshell with the correct variables
+"""
+
+from webshell_terminal import WebshellTerminal
+
+def main():
+
+    webshell = WebshellTerminal()
+    webshell.cmdloop()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
